f46ec628e8232af6918ea0d43e1daf35fd2d2f4c
Mohit <mmehndiratta@tavisca.com>
2018-10-21 13:25:10 +0530

    updated with user preferences

---
+++ TripAssistantSearchEngineApi/ActivityApi/ActivityTranslator.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using Core.Contracts;
+
+namespace TripAssistantSearchEngineApi
+{
+    public class ActivityTranslator : IActivityTranslator
+    {
+        readonly string url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
+        private static List<string> _staticPlaceType = new List<string>(new string[]{
+            "amusement_park",
+            "aquarium",
+            "art_gallery",
+            "church",
+            "hindu_temple",
+            "mosque",
+            "museum",
+            "park",
+            "shopping_mall",
+            "zoo",
+            "natural_feature",
+            "point_of_interest"
+        });
+
+        public List<Activity> GetFilteredActivity(List<JObject> activityjObject)
+        {
+            List<Activity> activityList = new List<Activity>();
+            Activity activity = new Activity();
+            List<ActivityList> tempData = new List<ActivityList>();
+            ActivityList activityList1 = new ActivityList();
+            JArray types = new JArray();
+
+            foreach (JObject rest in activityjObject)
+            {
+                var results = rest["results"].Value<JArray>();
+                activityList1 = new ActivityList();
+                activity = new Activity();
+                tempData = new List<ActivityList>();
+                foreach (JObject res in results)
+                {
+                    types = res["types"].Value<JArray>();
+                    foreach(string type in types)
+                    {
+                        if (_staticPlaceType.Contains(type))
+                        {
+                            activity.Type = type;
+                            break;
+                        }
+                    }
+                    activityList1 = SingleFilteredResult(res);
+                    tempData.Add(activityList1);  
+                }
+                activity.ListActivity = tempData;
+                if (activity.Type != null)
+                {
+                    activityList.Add(activity);
+                }
+            }
+            return activityList;
+        }
+        public ActivityList SingleFilteredResult(JObject res)
+        {
+            ActivityList activity = new ActivityList();
+            try {
+                
+                JObject activityDetails;
+                string placeId = res["place_id"].Value<String>();
+                activityDetails = res;
+                activity.Name = activityDetails["name"].Value<String>();
+                if (activityDetails["rating"] != null)
+                {
+                    activity.Rating = activityDetails["rating"].Value<Double>();
+                }
+                JObject photo = null;
+                JObject geometry = activityDetails["geometry"].Value<JObject>();
+                JObject location = geometry["location"].Value<JObject>();
+                activity.Lattitude = location["lat"].Value<Double>();
+                activity.Longitude = location["lng"].Value<Double>();
+                if (activityDetails["photos"] != null)
+                {
+                    JArray photoArray = res["photos"].Value<JArray>();
+                    photo = photoArray[0].Value<JObject>();
+                    activity.PhotoUrl = url + photo["photo_reference"].Value<String>() + "&key=AIzaSyD2bL_pYSzue4JkSDQg4fYSuVT8XA_bjCQ";
+                }
+                
+                activity.PlaceId = placeId;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                activity = null;
+            }
+            return activity;
+        }
+
+    }
+    
+}
---
+++ TripAssistantSearchEngineApi/UserPreferencesService/UserPreferencesService.cs
@@ -0,0 +1,41 @@
+﻿using Core.Contracts;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TripAssistantSearchEngineApi
+{
+    public class UserPreferencesService : IUserPreferenceService
+    {
+        List<Activities> activities = new List<Activities>()
+        {
+            new Activities {Points = 20, Type = "amusement_park"},
+            new Activities {Points = 5, Type = "aquarium"},
+            new Activities {Points = 0, Type = "art_gallery"},
+            new Activities {Points = 0, Type = "church"},
+            new Activities {Points = 0, Type = "hindu_temple"},
+            new Activities {Points = 0, Type = "mosque"},
+            new Activities {Points = 0, Type = "museum"},
+            new Activities {Points = 10, Type = "park"},
+            new Activities {Points = 25, Type = "shopping_mall"},
+            new Activities {Points = 30, Type = "zoo"},
+            new Activities {Points = 0, Type = "natural_feature"},
+            new Activities {Points = 20, Type = "point_of_interest"}
+        };
+        public List<Activity> GetFilteredResultsBasedOnUserPreferences(List<Activity> activityList)
+        {
+            List<Activity> filteredActivityResult = new List<Activity>();
+            List<Activities> list =  activities.OrderByDescending(x => x.Points).ToList();
+            foreach(Activities activitie in list)
+            {
+                foreach(Activity activity in activityList)
+                {
+                    if (activity.Type.Equals(activitie.Type))
+                    {
+                        filteredActivityResult.Add(activity);
+                    }
+                }
+            }
+            return filteredActivityResult;
+        }
+    }
+}
